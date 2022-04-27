647b827118fb9cfe3426a4797c4f9d227a48c25b
Tolgahan KARA <tolga-han-kara@hotmail.com>
2022-04-27 11:49:17 +0300

    CoderbyteExamplePrimeMover

---
+++ src/PrimeMover.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PrimeMover {
+
+    public static int PrimeMover(int num){
+
+        return num;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println(PrimeMover(scn.nextInt()));
+    }
+
+}
