c41fbcb5a605e91eb09e8fb01fdb777c1f011e59
Tolgahan KARA <tolga-han-kara@hotmail.com>
2022-04-27 12:10:14 +0300

    find prime number

--- src/PrimeMover.java
+++ src/PrimeMover.java
@@ -4,6 +4,19 @@ public class PrimeMover {
 
     public static int PrimeMover(int num){
 
+        int toplam = 0;
+        int asal = 0;
+
+        for (int i =2; i<num; i++){
+
+            if (num%i == 0){
+
+                toplam++;
+
+            }
+
+        }
+
         return num;
 
     }
